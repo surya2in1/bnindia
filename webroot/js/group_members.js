e046f8c377b6840461af3fa6e5acc34ac6b7c9f1
jayshri <jayshris22@gmail.com>
2022-04-03 11:44:23 +0530

    remaining3032022

--- webroot/js/group_members.js
+++ webroot/js/group_members.js
@@ -607,10 +607,14 @@ function refresh_member_table(){
 }
 
 function get_group_code(){
-	var total_number = $('#total_number').val();
-	var chit_amount = $('#chit_amount').val();
-	var created_by = $('#created_by').val();
-	var group_id = $('#id').val();
+	var total_number = parseInt($('#total_number').val());
+	var chit_amount = parseFloat($('#chit_amount').val());
+	var created_by = parseInt($('#created_by').val());
+	var group_id = parseInt($('#id').val());
+
+	if(total_number < 1){
+		return false;
+	}
 	$.ajax({
 			   "url": $('#router_url').val()+"Groups/getGroupCode/"+total_number+"/"+created_by+"/"+chit_amount+"/"+group_id,
 	            "type": "POST",
